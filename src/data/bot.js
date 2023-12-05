9031a947ae28dd930499ff54abc8f81a453627f4
Lord <71190020+yLorde@users.noreply.github.com>
2023-12-05 13:46:20 -0300

    Update bot.js

--- src/data/bot.js
+++ src/data/bot.js
@@ -1,6 +1,6 @@
 module.exports = {
-    clientId: '904863027982139435',
-    clientSecret: '5YytOy75gSF7cERZs4eE1L85m51wWJ9G',
-    addUrl: 'https://discord.com/api/oauth2/authorize?client_id=904863027982139435&permissions=8&scope=bot',
-    publicKey: '9856763b673f1d29e79d04937d41558684a55723584c4c55a0fe010630cbd752',
+    clientId: '',
+    clientSecret: '',
+    addUrl: '',
+    publicKey: '',
 }
